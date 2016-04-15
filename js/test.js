56914cb12de1fe8e706c7c10ecfd1e85205c01db
lindemann18 <ashernetz@gmail.com>
2016-04-15 08:42:14 -0500

    new files

--- js/test.js
+++ js/test.js
@@ -1,18 +1,34 @@
 var app=angular.module("app",[]);
 
 app.controller("myController",function($scope,$http,$q,contacts){
-	var cat = contacts.getAll();
-    cat.then(function(data) {
-        $scope.categories = data.data;//don't forget "this" in the service
-        console.log($scope.categories);
-        
-    $scope.person = $scope.categories[0];
-    console.log($scope.person);
-    })
+	$scope.ContactsService = contacts;
+	$scope.contactsData    = $scope.ContactsService.getAll();
+	$scope.selectedPerson  = {"email":null}; 
+	console.log($scope.ContactsService);
+    $scope.contactsData.then(function(data) {
+        $scope.contacts = data.data;//don't forget "this" in the service
+        console.log($scope.contacts);
+    });
+
+    $scope.selectPerson = function(person)
+    {
+    	if($scope.selectedPerson == person)
+    	{
+    		$scope.selectedPerson = null
+    	}else{$scope.selectedPerson = person;}
+    	
+    	console.log($scope.selectedPerson);
+    }
+
+    $scope.Editar = function(person)
+    {
+    	
+    }
 
 });
 
-app.service("contacts",function($http,$q){
+app.service("contacts",function($http){
+	 this.selectedPerson = "null";
 	 this.getAll = function() {
 
         return $http.get("persons.json").then(function(data){
